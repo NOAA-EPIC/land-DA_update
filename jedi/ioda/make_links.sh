3ec393fbc6b78dacb624d81c50e2c910d3e0d2f6
cameronbook <cameron.book@noaa.gov>
2023-02-05 17:04:09 +0000

    Update make_links scripts for IODA and fv3-jedi to include both Hera and Orion Paths.

--- jedi/ioda/make_links.sh
+++ jedi/ioda/make_links.sh
@@ -1,11 +1,15 @@
-#!/bin/ksh 
+#!/bin/bash
 
-if [ $# == 1 ]; then 
-        echo "setting jedi path to input $1"
-        jedi_path=$1
-else 
-        jedi_path="/scratch2/BMC/gsienkf/UFS-RNR/UFS-RNR-stack/external/ioda-bundle/"
-fi 
+if [[ $# == 1 ]]; then
+  echo "setting jedi path to input $1"
+  jedi_path=$1
+elif [[ $HOSTNAME == *"Orion"* ]]; then
+  jedi_path="/work/noaa/epic-ps/role-epic-ps/contrib/ioda-bundle"
+elif [[ $HOSTNAME == *"hfe"* ]]; then
+  jedi_path="/scratch1/NCEPDEV/nems/role.epic/contrib/ioda-bundle"
+else
+ echo "Cannot determine where to link IODA converter script. Please manually invoke make_links.sh"
+fi
 
-ln -s ${jedi_path}/iodaconv/src/land/imsfv3_scf2ioda.py . 
+ln -s ${jedi_path}/iodaconv/src/land/imsfv3_scf2ioda.py .
 
